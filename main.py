bce4beec94446a3448e5146138fb9153066991eb
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-14 23:45:59 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:43:36.853Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T20:45:58.666Z
+Commit: Fix memory leak in event handlers
