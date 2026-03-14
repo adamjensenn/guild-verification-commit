4c9467a58e08a4e6fa9e39c5fb6231e76322501d
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-14 23:45:07 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:43:48.166Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T20:45:07.542Z
+Commit: Add export functionality for reports
