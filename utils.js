d43381ca684c39d69638414cef89ef81a3c542bb
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-16 00:06:31 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:45:53.536Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:06:31.038Z
+Commit: Fix memory leak in event handlers
