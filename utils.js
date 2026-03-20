c92eccbed0a2028e10f91adc8092e18db9d82a8e
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-20 14:06:14 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T20:05:44.202Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-20T11:06:14.237Z
+Commit: Patch API endpoint error handling
