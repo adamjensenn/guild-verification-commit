98d3fb7ecc552c38bf53649d000050d85721d6c0
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-22 22:21:28 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-20T11:06:41.638Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-22T19:21:27.769Z
+Commit: Patch API endpoint error handling
