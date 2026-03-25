a78e44f281a9554b9fb1fc996b2d8bc7095fd1ab
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-03-26 01:05:25 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-23T20:26:35.975Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-25T22:05:24.559Z
+Commit: Resolve null pointer exception in user service
