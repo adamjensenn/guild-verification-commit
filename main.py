bbe20b96daee58a84e49a81af3c4d9c8a3a393d5
Barbunya <158317973+adamjensenn@users.noreply.github.com>
2026-04-03 12:02:45 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T06:23:33.101Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-03T09:02:44.702Z
+Commit: Introduce real-time notifications
